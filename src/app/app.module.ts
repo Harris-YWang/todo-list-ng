d032388e26b7310a3a3031fcbde42cc3ba70eb2a
Harris Wang <harris.wang@mail.com>
2019-08-26 22:56:20 +1000

    add todo component and about module and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ToComponent } from './to/to.component';
+import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToComponent
+    ToComponent,
+    TodoComponent
   ],
   imports: [
     BrowserModule
