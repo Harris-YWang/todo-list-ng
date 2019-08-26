40d963390d33050ee58ec892794bfbe813bea3a2
Harris Wang <harris.wang@mail.com>
2019-08-26 23:24:20 +1000

    fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ToComponent } from './to/to.component';
 import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToComponent,
     TodoComponent
   ],
   imports: [
