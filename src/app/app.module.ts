78a4719601ad1427978a1838b8cd3948dac26cc5
Harris Wang <harris.wang@mail.com>
2019-08-27 15:17:03 +1000

    remove useless component and add routing to nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { TodoComponent } from "./todo/todo.component";
+import { AboutComponent } from "./about/about.component";
 import { AboutModule } from "./about/about.module";
-import { InlineComponent } from "./inline/inline.component";
-import { LayoutModule } from './layout/layout.module';
+import { LayoutModule } from "./layout/layout.module";
+import { RouterModule, Routes } from "@angular/router";
 
+const appRoutes: Routes = [
+  { path: "todo", component: TodoComponent },
+  { path: "about", component: AboutComponent },
+  { path: "", redirectTo: "/todo", pathMatch: "full" }
+];
 @NgModule({
-  declarations: [AppComponent, TodoComponent, InlineComponent],
-  imports: [BrowserModule, AboutModule, LayoutModule],
+  declarations: [AppComponent, TodoComponent],
+  imports: [BrowserModule, AboutModule, LayoutModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent]
 })
