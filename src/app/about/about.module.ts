41c47a6f5aa1e5aada3ee31b25b68ffcf9084631
Harris Wang <harris.wang@mail.com>
2019-08-27 16:02:12 +1000

    divid routing to separate files and add lazy-load

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -1,16 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AboutComponent } from './about.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AboutComponent } from "./about.component";
+import { AboutRoutingModule } from './about.routing.module';
 
 @NgModule({
   declarations: [AboutComponent],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    AboutComponent,
-  ],
+  imports: [CommonModule, AboutRoutingModule],
+  exports: [AboutComponent]
 })
-export class AboutModule { }
+export class AboutModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,12 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { TodoComponent } from "./todo/todo.component";
-import { AboutComponent } from "./about/about.component";
-import { AboutModule } from "./about/about.module";
 import { LayoutModule } from "./layout/layout.module";
-import { RouterModule, Routes } from "@angular/router";
+import { AppRoutingModule } from './app.routing.module'
 
-const appRoutes: Routes = [
-  { path: "todo", component: TodoComponent },
-  { path: "about", component: AboutComponent },
-  { path: "", redirectTo: "/todo", pathMatch: "full" }
-];
 @NgModule({
   declarations: [AppComponent, TodoComponent],
-  imports: [BrowserModule, AboutModule, LayoutModule, RouterModule.forRoot(appRoutes)],
+  imports: [BrowserModule, LayoutModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
