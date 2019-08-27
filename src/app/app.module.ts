4147c399d3a1d556c2e5c6c86abd0790c05acff6
Harris Wang <harris.wang@mail.com>
2019-08-27 16:29:20 +1000

    create todo-list and create-todo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { TodoComponent } from "./todo/todo.component";
 import { LayoutModule } from "./layout/layout.module";
-import { AppRoutingModule } from './app.routing.module'
+import { AppRoutingModule } from './app.routing.module';
+import { CreateTodoComponent } from './create-todo/create-todo.component';
+import { TodoListComponent } from './todo-list/todo-list.component'
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent],
+  declarations: [AppComponent, TodoComponent, CreateTodoComponent, TodoListComponent],
   imports: [BrowserModule, LayoutModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
