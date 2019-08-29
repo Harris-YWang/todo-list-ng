2e821f542d7761cd91bad94415bf8666b067e2a2
Harris Wang <harris.wang@mail.com>
2019-08-29 15:51:48 +1000

    create form for create-todo component

--- src/app/create-todo/create-todo.component.ts
+++ src/app/create-todo/create-todo.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, AbstractControl } from "@angular/forms";
+import { Category as Cate } from "../interface";
+interface Category {
+  id: number;
+  name: string;
+}
 
 @Component({
-  selector: 'app-create-todo',
-  templateUrl: './create-todo.component.html',
-  styleUrls: ['./create-todo.component.scss']
+  selector: "app-create-todo",
+  templateUrl: "./create-todo.component.html",
+  styleUrls: ["./create-todo.component.scss"]
 })
 export class CreateTodoComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  categories: Category[] = [
+    { id: 0, name: Cate[0] },
+    { id: 1, name: Cate[1] },
+    { id: 2, name: Cate[2] }
+  ];
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.form = this.fb.group({
+      description: "",
+      category: "",
+      content: ""
+    });
   }
 
+  submit() {
+    console.log(this.form.value);
+  }
+
+  get description(): AbstractControl {
+    return this.form.get("description");
+  }
+
+  get category(): AbstractControl {
+    return this.form.get("category");
+  }
 }
